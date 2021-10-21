1957494c3013be85bb7596e7f3da250ad0c747ce
lukehedger <lh4@pm.me>
2021-10-21 16:17:43 +0100

    Add create commit status action

---
+++ .github/actions/create-commit-status/create-commit-status.js
@@ -0,0 +1,23 @@
+const { Octokit } = require("@octokit/action");
+
+const octokit = new Octokit();
+
+const createCommitStatus = async () => {
+  try {
+    const response = await octokit.request(
+      "POST /repos/{owner}/{repo}/statuses/{sha}",
+      {
+        context: process.env.CONTEXT,
+        owner: "lukehedger",
+        repo: "actions-workflow-run",
+        sha: process.env.SHA,
+        state: process.env.STATE,
+        target_url: process.env.TARGET_URL,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+createCommitStatus();
